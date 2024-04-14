395899d7b89bdca660a8497d842fb1bd77847b31
Harry Chen <harryhow@gmail.com>
2024-04-14 13:50:32 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,65 @@
+const { WebSocketServer } = require('ws')
+const http = require('http')
+const uuidv4 = require('uuid').v4
+const url = require('url')
+
+const server = http.createServer()
+const wsServer = new WebSocketServer({ server })
+
+const port = 8000
+const connections = {}
+const users = {}
+
+const handleMessage = (message, uuid) => {
+  const jsonMessage = JSON.parse(message)
+  console.log(`Received message: ${jsonMessage}`)
+  const user = users[uuid]
+  user.text = jsonMessage.text
+  user.role = jsonMessage.role
+  broadcast()
+
+  console.log(`Updated their updated text: ${user.text}`)
+}
+
+const handleClose = uuid => {
+  console.log(`Disconnected`)
+  delete connections[uuid]
+  delete users[uuid]
+  broadcast()
+}
+
+const broadcast = () => {
+  let teacherMessage;
+  Object
+    .keys(connections)
+    .forEach(uuid => {
+      if (users[uuid].role === 'teacher') {
+        teacherMessage = JSON.stringify(users[uuid].text);
+      }
+    });
+
+  if (teacherMessage) {
+    Object
+      .keys(connections)
+      .forEach(uuid => {
+        const connection = connections[uuid]
+        connection.send(teacherMessage)
+      });
+  }
+}
+
+wsServer.on('connection', (connection, request) => {
+  console.log(connection)
+  const uuid = uuidv4()
+  connections[uuid] = connection
+  users[uuid] = {
+    role: { },
+    text: { }
+  }
+  connection.on('message', message => handleMessage(message, uuid));
+  connection.on('close', () => handleClose(uuid));
+});
+
+server.listen(port, () => {
+  console.log(`WebSocket server is running on port ${port}`);
+})
