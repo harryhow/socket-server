0ab4077a0bffcd257550601a8ce0316368af93c8
Harry Chen <harryhow@gmail.com>
2024-04-22 05:40:25 +0800

    update by Austin

--- server.js
+++ server.js
@@ -14,16 +14,24 @@ const wsServer = new WebSocketServer({ server })
 const port = 8000
 const connections = {}
 const users = {}
+let currentIndex = 0;
+const votes = {};
 
 const handleMessage = (message, uuid) => {
   const jsonMessage = JSON.parse(message)
-  console.log(`Received message: ${jsonMessage}`)
+  console.log(`Received message: ${JSON.stringify(jsonMessage)}`)
   const user = users[uuid]
   user.text = jsonMessage.text
   user.role = jsonMessage.role
+  if (jsonMessage.text.type === 'vote') {
+    user.text.selectedOptionIndex = jsonMessage.text.selectedOptionIndex;
+    console.log(`Client ${uuid} voted: ${user.text.selectedOptionIndex}`);
+    votes[uuid] = user.text.selectedOptionIndex;
+    currentIndex = jsonMessage.text.currentIndex;
+  }
   broadcast()
 
-  console.log(`Updated their updated text: ${user.text}`)
+  console.log(`Updated their updated text: ${JSON.stringify(user.text)}`)
 }
 
 const handleClose = uuid => {
@@ -39,6 +47,12 @@ const broadcast = () => {
     .keys(connections)
     .forEach(uuid => {
       if (users[uuid].role === 'teacher') {
+        const voteCounts = Object.values(votes).reduce((counts, vote) => {
+          counts[vote] = (counts[vote] || 0) + 1;
+          return counts;
+        }, {});
+        users[uuid].text.currentIndex = currentIndex;
+        users[uuid].text.votes = voteCounts;
         teacherMessage = JSON.stringify(users[uuid].text);
       }
     });
